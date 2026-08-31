6a5bf4e7ebe6fc40af54156f181400a9317036ee
ViralForgeStudio <totscaca04@gmail.com>
2026-08-31 05:39:52 -0300

    Create login.js

---
+++ api/tiktok/login.js
@@ -0,0 +1,22 @@
+export default function handler(req, res) {
+  const state = crypto.randomUUID();
+
+  res.setHeader(
+    "Set-Cookie",
+    `tiktok_state=${state}; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=600`
+  );
+
+  const params = new URLSearchParams({
+    client_key: process.env.TIKTOK_CLIENT_KEY,
+    scope: "user.info.basic,video.publish,video.upload",
+    response_type: "code",
+    redirect_uri: "https://viralforge-website.vercel.app/callback/",
+    state
+  });
+
+  res.writeHead(302, {
+    Location: `https://www.tiktok.com/v2/auth/authorize/?${params.toString()}`
+  });
+
+  res.end();
+}
